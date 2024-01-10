bf28b4fb80dfbaebdadb2473a2eee9b08939cb37
mabroukissam <145654259+MabroukIssam@users.noreply.github.com>
2024-01-10 21:24:31 +0100

    Highliting Grids element In this commit: * a button is created to interract with the user and to choose the grid length * when hoovered the background of an element is changing.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,39 @@
 console.log("this is sketch.js script");
 let gridLength = 4;
+const button = document.querySelector("#gridNumber");
+const buttonParentNode = button.parentElement;
 
-const container = document.querySelector("#main-container");
-for (let i = 0; i < gridLength; i++) {
-  const rowElement = document.createElement("div");
-  rowElement.classList.add("row-container");
-  for (let j = 0; j < gridLength; j++) {
-    const colElement = document.createElement("div");
-    colElement.classList.add("grid-element");
-    rowElement.appendChild(colElement);
+createGrid();
+button.addEventListener("click", () => {
+  gridLength = prompt(`Enter The grid length 
+    It must be an integer betwee 1 and 100
+    ;)`);
+
+  if (
+    Number.isInteger(Number(gridLength)) &&
+    gridLength > 0 &&
+    gridLength <= 100
+  ) {
+    resetGrid();
+    createGrid();
   }
-  container.appendChild(rowElement);
+});
+function resetGrid() {
+  const containerToDelete = document.querySelector("#main-container");
+  containerToDelete.remove();
+}
+function createGrid() {
+  const container = document.createElement("div");
+  container.setAttribute("id", "main-container");
+  buttonParentNode.insertBefore(container, button);
+  for (let i = 0; i < gridLength; i++) {
+    const rowElement = document.createElement("div");
+    rowElement.classList.add("row-container");
+    for (let j = 0; j < gridLength; j++) {
+      const colElement = document.createElement("div");
+      colElement.classList.add("grid-element");
+      rowElement.appendChild(colElement);
+    }
+    container.appendChild(rowElement);
+  }
 }
