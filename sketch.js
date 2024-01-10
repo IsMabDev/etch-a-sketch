f00fc8c0ceb9a0e2ce0ec65880e5b77a7144f982
mabroukissam <145654259+MabroukIssam@users.noreply.github.com>
2024-01-10 16:12:50 +0100

    Using javascript to create the grid.
    The number is entred by the program
    New row containers are created using the length of the grid
    Each row container will contain the same number of col container
    The display is managed by the css

--- sketch.js
+++ sketch.js
@@ -1,1 +1,14 @@
 console.log("this is sketch.js script");
+let gridLength = 4;
+
+const container = document.querySelector("#main-container");
+for (let i = 0; i < gridLength; i++) {
+  const rowElement = document.createElement("div");
+  rowElement.classList.add("row-container");
+  for (let j = 0; j < gridLength; j++) {
+    const colElement = document.createElement("div");
+    colElement.classList.add("grid-element");
+    rowElement.appendChild(colElement);
+  }
+  container.appendChild(rowElement);
+}
