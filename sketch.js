e0a61742cc3b2cc1aed53de861688e65db0f6ccb
mabroukissam <145654259+MabroukIssam@users.noreply.github.com>
2024-01-13 09:58:51 +0100

    All the functions are implemented * The user can choose the original and the hovered color of the grid using an input color selector * three buttons are used to change the behavior of the grid

--- sketch.js
+++ sketch.js
@@ -1,7 +1,67 @@
-console.log("this is sketch.js script");
-//The initial grid is 50 length
-let gridLength = 50;
+//The initial grid is 64 length
+let gridLength = 4;
+let defaultGridColor = "#0000ff";
+let colorToChange = "#ff0000";
+let userChoice = changeBackgroundToUserColor;
+
+//This function allow the interraction with the user to change the behavior
+function changeBehavior(callback) {
+  callback();
+}
+
+const behaviorButtons = document.querySelector("#behaviorButtons");
+behaviorButtons.addEventListener("click", (event) => {
+  switch (event.target.id) {
+    case "hoveredButton":
+      userChoice = changeBackgroundToUserColor;
+      resetGrid();
+      createGrid();
+      break;
+    case "rainbowButton":
+      userChoice = changeBackgroundToRainbow;
+      resetGrid();
+      createGrid();
+      break;
+    case "darkerButton":
+      userChoice = changeBackgroundToDarker;
+      resetGrid();
+      createGrid();
+      break;
+  }
+});
+
+//backgrounds of behavior buttons
+
+const hoveredButton = document.querySelector("#hoveredButton");
+const darkerButton = document.querySelector("#darkerButton");
+
+function updateButtons() {
+  hoveredButton.style.backgroundColor = defaultGridColor;
+  hoveredButton.style.color = colorToChange;
+  darkerButton.style.background = `linear-gradient(${defaultGridColor},black)`;
+  darkerButton.style.color = colorToChange;
+}
+
 createGrid();
+
+//add an event handler for choosing the user's default colors
+const userColor = document.querySelector("#userDefaultColor");
+userColor.setAttribute("value", defaultGridColor);
+userColor.addEventListener("input", (event) => {
+  userDefaultColor(event.target.value);
+  resetGrid();
+  createGrid();
+});
+
+//add an event handler for choosing the user's hovered colors
+const userHoveredColor = document.querySelector("#userHoveredColor");
+userHoveredColor.setAttribute("value", colorToChange);
+userHoveredColor.addEventListener("input", (event) => {
+  userChangeColor(event.target.value);
+  resetGrid();
+  createGrid();
+});
+
 //adding an event to the button to change the grid length
 const button = document.querySelector("#gridNumber");
 
@@ -21,39 +81,94 @@ button.addEventListener("click", () => {
   }
 });
 
-//This function remove the container of the grid
+//This function removes the container of the grid
 function resetGrid() {
   const containerToDelete = document.querySelector("#main-container");
   containerToDelete.remove();
 }
 
-//This function create a grid depending on the gridLength
+//This function creates a grid depending on the gridLength
 function createGrid() {
-  const body = document.querySelector("body");
+  const gridContainer = document.querySelector("#gridContainer");
   const container = document.createElement("div");
   container.setAttribute("id", "main-container");
-  body.appendChild(container);
+  gridContainer.appendChild(container);
   for (let i = 0; i < gridLength; i++) {
     const rowElement = document.createElement("div");
     rowElement.classList.add("row-container");
     for (let j = 0; j < gridLength; j++) {
       const colElement = document.createElement("div");
       colElement.classList.add("grid-element");
+      colElement.style.backgroundColor = defaultGridColor;
       colElement.setAttribute("id", "div" + i + j);
       rowElement.appendChild(colElement);
     }
     container.appendChild(rowElement);
-    changeBackgroundToRed();
   }
+  updateButtons();
+
+  changeBehavior(userChoice);
 }
 
-//This function is to change the background when hovered
-function changeBackgroundToRed() {
-  const gridElement = document.querySelectorAll(".grid-element");
+//This function is to change the background to the user chosen color of each element when hovered
+function changeBackgroundToUserColor() {
+  const gridElements = document.querySelectorAll(".grid-element");
 
-  gridElement.forEach((item) => {
+  gridElements.forEach((item) => {
     item.addEventListener("mouseover", (event) => {
-      item.style.backgroundColor = "red";
+      item.style.backgroundColor = colorToChange;
     });
   });
+}
+
+function changeBackgroundToRainbow() {
+  const gridElements = document.querySelectorAll(".grid-element");
+
+  //a random color is calculated each time the element is hovered
+  gridElements.forEach((item) => {
+    item.addEventListener("mouseover", (event) => {
+      item.style.backgroundColor =
+        "rgb(" +
+        Math.floor(Math.random() * 255) +
+        "," +
+        Math.floor(Math.random() * 255) +
+        "," +
+        Math.floor(Math.random() * 255) +
+        ")";
+    });
+  });
+}
+
+function changeBackgroundToDarker() {
+  const gridElements = document.querySelectorAll(".grid-element");
+  //To get rgb values from the default color
+  const rgbFromElement = getComputedStyle(gridElements[0]).backgroundColor;
+
+  //get the rgb from the current element and calculate the incrementation for each color
+  const rgbValues = rgbFromElement.match(/\d+/g);
+  const redInc = Math.floor(rgbValues[0] / 10);
+  const greenInc = Math.floor(rgbValues[1] / 10);
+  const blueInc = Math.floor(rgbValues[2] / 10);
+
+  //depending on the current color, the amount of darkness is incremented by 10%
+  gridElements.forEach((item) => {
+    item.addEventListener("mouseover", (event) => {
+      const currentRGB = getComputedStyle(item).backgroundColor;
+      const rgbFromCurrent = currentRGB.match(/\d+/g);
+      const newRed = Number(rgbFromCurrent[0]) - redInc;
+      const newGreen = Number(rgbFromCurrent[1]) - greenInc;
+      const newBlue = Number(rgbFromCurrent[2]) - blueInc;
+
+      item.style.backgroundColor =
+        "rgb(" + newRed + "," + newGreen + "," + newBlue + ")";
+    });
+  });
+}
+
+function userDefaultColor(color) {
+  defaultGridColor = color;
+}
+
+function userChangeColor(color) {
+  colorToChange = color;
 }
